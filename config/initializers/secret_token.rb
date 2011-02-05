e3381abda77245a1f28b67eaea320f31291760c2
ianmorgan <ian.j.morgan@gmail.com>
2011-02-05 22:21:12 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wikikeep::Application.config.secret_token = 'c90d7c9709d41efb28ab670ba9fa0c2e9404fde0de69a3d0a72ba51f13a1e9bb3303bb4aa83105b60a6357c853a30b39531c601addbbac59bac0fd93edad9d7b'
