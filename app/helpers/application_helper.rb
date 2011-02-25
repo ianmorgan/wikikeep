e960e2713523dca3e97814755cfa32536d9efa4f
ianmorgan <ian.j.morgan@gmail.com>
2011-02-25 12:00:17 +1100

    Fixed problem with home page not showing for logged in users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #duplicate of method on ApplicationController made available views
+  def account_name 
+    session[:current_user][:account_name]
+  end
+
+  #duplicate of method on ApplicationController made available views
+  def account_id 
+    session[:current_user][:account_id]
+  end
+
 end
